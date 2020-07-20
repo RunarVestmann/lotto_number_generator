ac860cf0d067564f4b7f242f41ba68c5d159d612
RunarVestmann <runarvestmann@gmail.com>
2020-07-20 10:53:09 +0000

    Refactor

--- lotto_number_generator.py
+++ lotto_number_generator.py
@@ -1,31 +1,23 @@
 from bs4 import BeautifulSoup
-import requests as r
+import requests
 
 import random
 
-NUMBERS_PER_LOTTO_ROW = 6
+LOTTO_URL = 'https://games.lotto.is/result/lotto-statistics'
+VIKING_LOTTO_URL = 'https://games.lotto.is/result/vikinga-statistics'
+
 NUMBERS_PER_JOKER_ROW = 5
 
-def generate_row_non_repeating(a_dict, num_per_row):
+def generate_row(a_dict, num_per_row, repeating):
     row = []
     keys = list(a_dict.keys())
     values  = list(a_dict.values())
 
     for _ in range(num_per_row):
         number = random.choices(keys, values)[0]
-        values.remove(a_dict[number])
-        keys.remove(number) 
-        row.append(str(number))
-
-    return row
-
-def generate_row_repeating(a_dict, num_per_row):
-    row = []
-    keys = list(a_dict.keys())
-    values  = list(a_dict.values())
-
-    for _ in range(num_per_row):
-        number = random.choices(keys, values)[0]
+        if not repeating:
+            values.remove(a_dict[number])
+            keys.remove(number)
         row.append(str(number))
 
     return row
@@ -43,53 +35,85 @@ def fill_dict(a_dict, data):
     for i in range(0, len(data)-2, 2):
         a_dict[int(data[i].text.strip())] = int(data[i+1].text.strip())
 
-lotto_url = 'https://games.lotto.is/result/lotto-statistics'
-viking_lotto_url = 'https://games.lotto.is/result/vikinga-statistics'
+def get_url():
+    choice = get_user_choice()
 
-url = ''
-choice = get_user_choice()
-if choice.startswith('l'):
-    url = lotto_url
-    NUMBERS_PER_LOTTO_ROW = 5
-elif choice.startswith('v'):
-    url = viking_lotto_url
-    NUMBERS_PER_LOTTO_ROW = 6
+    url = ""
 
-html = r.get(url).text
+    if choice.startswith('l'):
+        url = LOTTO_URL
+    elif choice.startswith('v'):
+        url = VIKING_LOTTO_URL
+    
+    return url
 
-soup = BeautifulSoup(html, 'html.parser')
+def get_number_per_lotto_row(url):
+    numbers_per_lotto_row = 5
 
-lotto_dict = dict()
-lotto_data = soup.find('tbody').find_all('td')
-fill_dict(lotto_dict, lotto_data)
+    if url == LOTTO_URL:
+        numbers_per_lotto_row = 5
+    elif url == VIKING_LOTTO_URL:
+        numbers_per_lotto_row = 6
 
-joker_data = soup.find_all('tbody')[1].find_all('td')
-joker_dict = dict()
-fill_dict(joker_dict, joker_data)
+    return numbers_per_lotto_row
 
-viking_number_dict = dict()
-for i in range(1,9):
-    viking_number_dict[i] = lotto_dict[i]
+def get_soup(url):   
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'html.parser')
 
-lotto_row_count = int(input('How many lotto rows do you want: '))
-joker_row_count = int(input('How many joker rows do you want: '))
+def get_dict(soup, is_lotto_row):
+    a_dict = dict()
 
-lotto_rows = []
-for _ in range(lotto_row_count):
-    lotto_rows.append(generate_row_non_repeating(lotto_dict, NUMBERS_PER_LOTTO_ROW))
+    if is_lotto_row:
+        data = soup.find('tbody').find_all('td')
+    else:
+        data = soup.find_all('tbody')[1].find_all('td')
 
-joker_rows = []
-for _ in range(joker_row_count):
-    joker_rows.append(generate_row_repeating(joker_dict, NUMBERS_PER_JOKER_ROW))
+    fill_dict(a_dict, data)
 
-with open('lotto.txt', 'a+') as l:
-    for row in lotto_rows:
-        l.write(' '.join(row))
-        if choice.startswith('v'):
-            l.write(f' {generate_row_repeating(viking_number_dict, 1)[0]}')
-        l.write('\n')
+    return a_dict
 
-with open('joker.txt', 'a+') as j:
-    for row in joker_rows:
-        j.write(' '.join(row))
-        j.write('\n')
+def get_viking_dict(lotto_dict, url):
+    if url != VIKING_LOTTO_URL:
+        return None
+
+    viking_dict = dict()
+    for i in range(1,9):
+        viking_dict[i] = lotto_dict[i]
+
+    return viking_dict
+
+def get_rows(row_count, numbers_per_row, a_dict, repeating):
+    rows = []
+    for _ in range(row_count):
+        rows.append(generate_row(a_dict, numbers_per_row, repeating))
+
+    return rows
+
+def write_rows_to_file(filename, rows, viking_dict=None):
+    with open(filename, 'a+') as f:
+        for row in rows:
+            f.write(' '.join(row))
+            if viking_dict != None:
+                f.write(f' {generate_row(viking_dict, 1, True)[0]}')
+            f.write('\n')
+
+def main():
+    url = get_url()
+    numbers_per_lotto_row = get_number_per_lotto_row(url)
+    soup = get_soup(url)
+    
+    lotto_dict = get_dict(soup, False)
+    joker_dict = get_dict(soup, True)
+    viking_dict = get_viking_dict(lotto_dict, url)
+    
+    lotto_row_count = int(input('How many lotto rows do you want: '))
+    joker_row_count = int(input('How many joker rows do you want: '))
+
+    lotto_rows = get_rows(lotto_row_count, numbers_per_lotto_row, lotto_dict, False)
+    joker_rows = get_rows(joker_row_count, NUMBERS_PER_JOKER_ROW, joker_dict, True)
+
+    write_rows_to_file("lotto.txt", lotto_rows,viking_dict)
+    write_rows_to_file("joker.txt", joker_rows)
+
+main()
